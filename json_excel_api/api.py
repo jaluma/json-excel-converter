8ff23788dc9fe96ef7194bfe6c6cfeab6c94abe5
Javier Martínez <javiermartinezalvarez98@gmail.com>
2022-12-31 19:38:01 +0100

    add api model

--- json_excel_api/api.py
+++ json_excel_api/api.py
@@ -1,13 +1,16 @@
+from functools import reduce
+
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 from json_excel_api.config import Settings
+from json_excel_api.model import ExcelRequest, ExcelFormatDefinition, ExcelHeaderInfoDefinition
 
-from json_excel_converter import Converter 
+from json_excel_converter import Converter
 from json_excel_converter.xlsx import DEFAULT_COLUMN_WIDTH, DEFAULT_ROW_HEIGHT, Writer
-from json_excel_converter.xlsx.formats import FontColor, LastUnderlined, Bold, \
+from json_excel_converter.xlsx.formats import LastUnderlined, Bold, \
     Centered, Format
 
 import random
@@ -16,44 +19,68 @@ import string
 app = FastAPI(docs_url=Settings.docs_url, redoc_url=Settings.redoc_url)
 app.mount("/static", StaticFiles(directory="json_excel_api/static"), name="static")
 
+
 @app.get("/")
 def index():
     return {"Hello": "World"}
 
+
 @app.post("/")
-def create_item():
+async def create_item(model: ExcelRequest):
     excel_file_path = f'/tmp/{random_name(16)}.xlsx'
-    w = Writer(excel_file_path,
-            header_formats=(
-               Centered,
-               Bold,
-               LastUnderlined,
-               #FontColor('red')
-            ),
-            data_formats=(
-               #FontColor('black'),
-            ),
-            column_widths={
-                DEFAULT_COLUMN_WIDTH: 20
-            },
-            row_heights={
-                DEFAULT_ROW_HEIGHT: 20
-            }
-        )
 
-    data = [
-        {'a': 'Hello'},
-        {'a': 'World'}
-    ]
+    workbook = None
+    for worksheet in model.data:
+        w = Writer(excel_file_path,
+                   workbook=workbook,
+                   sheet_name=worksheet.name,
+                   header_formats=[map_format(format_header) for format_header in
+                                   filter(lambda x: x is not None, worksheet.definition.formats or [])],
+                   data_formats=[map_format(format_data) for format_data in
+                                 filter(lambda x: x is not None, worksheet.formats or [])],
+                   column_widths=dict({DEFAULT_COLUMN_WIDTH: worksheet.definition.default_column_width}.items()
+                                      | { header.name: header.column_width
+                                          for header in filter(lambda x: x is not None, worksheet.definition.headers or []) }.items()
+                                      ),
+                   row_heights={
+                       DEFAULT_ROW_HEIGHT: worksheet.definition.default_row_height
+                   },
+                   global_configs=(model.global_settings or {}).__dict__
+                )
 
-    conv = Converter()
-    conv.convert(data, w)
+        conv = Converter()
+        conv.convert(worksheet.data, w)
 
-    headers = {'Content-Disposition': 'attachment; filename="Book.xlsx"'}
+        if not workbook:
+            workbook = w.workbook
+
+    workbook.close()
+
+    headers = {f'Content-Disposition': f'attachment; filename="{model.filename}.xlsx"'}
     return FileResponse(excel_file_path, headers=headers)
+
+
+def map_format(header: ExcelFormatDefinition):
+    if header is None:
+        return
+
+    return Format({
+        header.key: header.value
+    })
+
+
+def map_column_config(header: ExcelHeaderInfoDefinition):
+    if header is None:
+        return
+
+    return {
+        header.name: header.column_width
+    }
+
 
 def random_name(length: int):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
 
 def custom_openapi():
     if app.openapi_schema:
@@ -66,4 +93,6 @@ def custom_openapi():
     )
     app.openapi_schema = openapi_schema
     return app.openapi_schema
+
+
 app.openapi = custom_openapi
---
+++ json_excel_api/model.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from typing import Any, Optional, Union
+from pydantic import BaseModel
+
+
+@dataclass
+class ExcelSettingDefinition:
+    strings_to_numbers: bool = False
+    strings_to_formulas: bool = True
+    strings_to_urls: bool = True
+    nan_inf_to_errors: bool = False
+    default_date_format: str = None
+    remove_timezone: bool = False
+    use_future_functions: bool = False
+    max_url_length: int = 0
+
+
+@dataclass
+class ExcelFormatDefinition:
+    key: str
+    value: object
+
+
+@dataclass
+class ExcelHeaderType:
+    type: str
+    format: Union[str, None] = None       # e.g. number ->   #.##
+    data: Union[object, None] = None   # e.g. currency -> symbol EUR
+
+
+@dataclass
+class ExcelHeaderInfoDefinition:
+    name: str
+    type: ExcelHeaderType
+    column_width: Optional[float] = 20
+
+
+@dataclass
+class ExcelHeaderDefinition:
+    headers: list[ExcelHeaderInfoDefinition]
+    formats: Union[list[ExcelFormatDefinition], None] = None
+    default_column_width: Optional[float] = 20
+    default_row_height: Optional[float] = 20
+
+
+@dataclass
+class ExcelWorksheetDefinition:
+    name: str
+    definition: ExcelHeaderDefinition
+    data: object
+    formats: Union[list[ExcelFormatDefinition], None] = None
+
+
+@dataclass
+class ExcelRequest:
+    filename: str
+    data: list[ExcelWorksheetDefinition]
+    global_settings: Union[ExcelSettingDefinition, None] = None
